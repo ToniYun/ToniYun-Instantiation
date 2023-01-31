f8e66f0b731f5fc3b2deca4bdeaf873816274bfc
ToniYun <antonioyun2000@gmail.com>
2023-01-31 20:40:03 +0000

    final

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object hello = new Object();
+        return hello;
     }
 }
